ebac1b8d3a346473c4b4d366188c72ebfb7aa896
Igor12x <igorbtenorioidi@gmail.com>
2023-03-03 21:12:38 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projetotcc/Tela_Cadastro.java
@@ -0,0 +1,14 @@
+package com.example.projetotcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_Cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro);
+    }
+}
