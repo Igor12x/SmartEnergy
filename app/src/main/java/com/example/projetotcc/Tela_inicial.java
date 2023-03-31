f71e77e3911bc33992ba2df6f2958d8162b242ac
bruKorczak <bkorczak45@gmail.com>
2023-03-31 19:09:29 -0300

    [feat]-Tela inicio parte superior: Criacao do design do grafico

---
+++ app/src/main/java/com/example/projetotcc/Tela_inicial.java
@@ -0,0 +1,14 @@
+package com.example.projetotcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_inicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+}
