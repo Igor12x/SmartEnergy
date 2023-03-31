82262a3150469b14ae7d11559d143605d4ddf359
Feliciano81 <gfelicosta654@gmail.com>
2023-03-30 21:43:34 -0300

    [feat] - Tela Senha Alterada com Sucesso: Criação do desing

---
+++ app/src/main/java/com/example/projetotcc/Tela_Recuperacao_Senha.java
@@ -0,0 +1,15 @@
+package com.example.projetotcc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Tela_Recuperacao_Senha extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_recuperecao_senha);
+    }
+}
+
+
