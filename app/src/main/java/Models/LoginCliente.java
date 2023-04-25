4dccfd9c61dc9a766e370082dcc54d4830f15e1a
JosephSantos742 <josephsantos742@gmail.com>
2023-04-24 22:14:49 -0300

    [feat] Model LoginCliente criada

---
+++ app/src/main/java/Models/LoginCliente.java
@@ -0,0 +1,12 @@
+package Models;
+
+public class LoginCliente {
+    private String cpf;
+    private String senha;
+
+
+    public LoginCliente(String cpf, String senha) {
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+}
--- app/src/main/java/com/example/projetotcc/Tela_Login.java
+++ app/src/main/java/com/example/projetotcc/Tela_Login.java
@@ -3,12 +3,17 @@ package com.example.projetotcc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Tela_Login extends AppCompatActivity {
+    private TextView txtCpf, txtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+
+        txtCpf = findViewById(R.id.txtCpf);
+        txtSenha = findViewById(R.id.txtSenha);
     }
 }
