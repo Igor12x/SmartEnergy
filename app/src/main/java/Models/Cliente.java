63ecd05a46218d14bf92e36b965cef96dfd43b38
Feliciano81 <gfelicosta654@gmail.com>
2023-04-26 21:27:21 -0300

    [feat] - API Cadastro Cliente: unificação com Android

--- app/src/main/java/Models/Cliente.java
+++ app/src/main/java/Models/Cliente.java
@@ -2,6 +2,8 @@ package Models;
 
 public class Cliente {
     private String nome;
+
+    private String senha;
     private String cpf;
     private String email;
     private String telefone;
@@ -13,9 +15,19 @@ public class Cliente {
         this.telefone = telefone;
     }
 
+    public Cliente(String nome, String senha, String cpf, String email, String telefone) {
+        this.nome = nome;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
     public String getNome() {
         return nome;
     }
+
+    public String getSenha() {return senha;}
 
     public String getCpf() {
         return cpf;
