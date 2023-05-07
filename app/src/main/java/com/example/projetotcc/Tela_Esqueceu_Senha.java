358492346ad0a56b6b44d9cf268746022122eb04
bruKorczak <bkorczak45@gmail.com>
2023-05-07 19:40:01 -0300

    [fix] tela esqueci a senha  - alteração de img para buttonImage sobre a seta de volta para tela Login.

--- app/src/main/java/com/example/projetotcc/Tela_Esqueceu_Senha.java
+++ app/src/main/java/com/example/projetotcc/Tela_Esqueceu_Senha.java
@@ -2,13 +2,31 @@ package com.example.projetotcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Tela_Esqueceu_Senha extends AppCompatActivity {
+
+    private ImageButton btnVoltaEsqueci;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_esqueceu_senha);
+
+        //referencias
+        btnVoltaEsqueci = findViewById(R.id.btnVoltaEsqueci);
+
+        //voltando para tela de inicio principal
+        btnVoltaEsqueci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Tela_Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
