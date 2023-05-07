17e05488d9e942bb8c710bb003a5cb5d2f955247
bruKorczak <bkorczak45@gmail.com>
2023-05-07 19:52:36 -0300

    [fix] tela Criar nova senha  - alteração de img para buttonImage sobre a seta de volta para tela Código de verificação.

--- app/src/main/java/com/example/projetotcc/Tela_Criar_Nova_Senha.java
+++ app/src/main/java/com/example/projetotcc/Tela_Criar_Nova_Senha.java
@@ -2,13 +2,29 @@ package com.example.projetotcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Tela_Criar_Nova_Senha extends AppCompatActivity {
 
+    private ImageButton btnVoltaNovaSenha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_criar_nova_senha);
+
+        //referencias
+        btnVoltaNovaSenha = findViewById(R.id.btnVoltaNovaSenha);
+
+        //voltando para tela de Código verifição
+        btnVoltaNovaSenha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Tela_Principal.class);
+                startActivity(intent);
+            }
+        });
     }
 }
