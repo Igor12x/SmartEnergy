cda46de5711a691c5a740ccb859d64cd19c76017
bruKorczak <bkorczak45@gmail.com>
2023-05-07 21:45:56 -0300

    [fix] tela Perfil - Front está pronto, apenas falta mudança de icon

--- app/src/main/java/com/example/projetotcc/Tela_Perfil.java
+++ app/src/main/java/com/example/projetotcc/Tela_Perfil.java
@@ -2,13 +2,30 @@ package com.example.projetotcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Tela_Perfil extends AppCompatActivity {
+
+    private ImageButton btnVoltaPerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_perfil);
+
+        //referencias
+        btnVoltaPerfil = findViewById(R.id.btnVoltaPerfil);
+
+        //voltando para tela de inicio principal
+        btnVoltaPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Tela_Principal.class);
+                startActivity(intent);
+            }
+        });
     }
 }
