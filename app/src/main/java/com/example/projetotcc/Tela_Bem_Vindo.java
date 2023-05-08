6ff1b7a4a2def2e0fda1508a21290ee945d759e4
bruKorczak <bkorczak45@gmail.com>
2023-05-07 22:09:37 -0300

    [fix] tela Bem vindo - está pronta

---
+++ app/src/main/java/com/example/projetotcc/Tela_Bem_Vindo.java
@@ -0,0 +1,30 @@
+package com.example.projetotcc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Tela_Bem_Vindo extends AppCompatActivity {
+    private Button btnBemVindo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_bem_vindo);
+
+        //Referencias
+        btnBemVindo = findViewById(R.id.btnBemVindo);
+
+        //Volte a tela de login
+        btnBemVindo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Tela_Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
