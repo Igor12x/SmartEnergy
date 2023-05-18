572d14b17b3b31d3b8a669d7e4d72ddc49bb47d2
bruKorczak <bkorczak45@gmail.com>
2023-05-18 02:00:41 -0300

    [feat] layout_dialog.xml Tela_Perfil border.xml - front e back do alertdialog desconectar está pronto

--- app/src/main/java/com/example/projetotcc/Tela_Perfil.java
+++ app/src/main/java/com/example/projetotcc/Tela_Perfil.java
@@ -2,6 +2,7 @@ package com.example.projetotcc;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 import android.content.DialogInterface;
@@ -9,13 +10,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class Tela_Perfil extends AppCompatActivity {
 
     private ImageButton btnVoltaPerfil, ibtnDesconectar;
+    //private Button btnSim, btnNao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,44 +31,52 @@ public class Tela_Perfil extends AppCompatActivity {
         btnVoltaPerfil = findViewById(R.id.btnVoltaPerfil);
         ibtnDesconectar = findViewById(R.id.ibtnDesconectar);
 
-        //limite
+        //Desconectar
         ibtnDesconectar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(Tela_Perfil.this);
-                builder.setTitle("Desconectar");
-                builder.setMessage("Deseja sair mesmo da conta");
-                builder.setPositiveButton("Sair", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // ação a ser executada ao clicar no botão positivo
-                        Handler atraso = new Handler();
-                        atraso.postDelayed(new Runnable() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Tela_Perfil.this, R.style.AlertDialogTheme);
+                View view1 = LayoutInflater.from(Tela_Perfil.this).inflate(
+                        R.layout.layout_dialog,(ConstraintLayout)findViewById(R.id.layoutDialogContainer)
+                );
+                builder.setView(view1);
+                final AlertDialog alertDialog = builder.create();
+
+                Button btnSim = view1.findViewById(R.id.btnSim);
+                btnSim.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
                             @Override
                             public void run() {
-                                Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+                                alertDialog.dismiss();
+
+                                SharedPreferences prefs = getSharedPreferences("Usuario", Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = prefs.edit();
+                                editor.clear();
+                                editor.apply();
+
+                                Intent intent = new Intent(getApplicationContext(), Tela_Login.class);
+                                startActivity(intent);
                             }
-                        }, 2500);
-                        dialog.dismiss();
-
-                        SharedPreferences prefs = getSharedPreferences("Usuario", Context.MODE_PRIVATE);
-                        SharedPreferences.Editor editor = prefs.edit();
-                        editor.clear();
-                        editor.apply();
-
-
-                        Intent intent = new Intent(getApplicationContext(), Tela_Login.class);
-                        startActivity(intent);
+                        }, 500); // ajuste o valor do atraso conforme necessário
                     }
                 });
-                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // ação a ser executada ao clicar no botão negativo
+
+                Button btnNao = view1.findViewById(R.id.btnNao);
+                btnNao.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertDialog.dismiss();
                     }
                 });
-                AlertDialog dialog = builder.create();
-                dialog.show();
+
+
+                alertDialog.show();
             }
         });
+
 
 
         //------------------------------------------------------------------------------------
