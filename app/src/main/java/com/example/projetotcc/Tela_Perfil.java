07474ec312350dc2effe55a837b99980eb5c456b
Igor12x <igorbtenorioidi@gmail.com>
2023-05-20 14:56:39 -0300

    [fix] adicionei duas variaveis globais para trabalhar nas alterações que fiz na lógica do metódo do botão ImgBtnEdit. Coloquei uma validação caso o usuario não realizar alterações. renomeei o metódo alterarCadastro para editarCadastro. Coloquei todos os metódo necessário como private. Adicionei try-catch no carregarDadosResidencia(), editarCadastro(), carregarDadosPerfil() e no getIdClienteSharedPreferences()

--- app/src/main/java/com/example/projetotcc/Tela_Perfil.java
+++ app/src/main/java/com/example/projetotcc/Tela_Perfil.java
@@ -1,7 +1,5 @@
 package com.example.projetotcc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,6 +10,9 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -28,11 +29,14 @@ import Models.ResidenciaAdapter;
 public class Tela_Perfil extends AppCompatActivity {
     private TextView txtNome;
     private AlterarCadastro dados;
+    private String emailSemAlteracao = null;
+    private String telSemAlteracao = null;
     private Spinner spinnerCasas;
     private SharedPreferences ler;
     private ImageButton btnVoltaPerfil, imgBtnEdit;
     private EditText plainPerfilE, plainPerfilTel, plainCEPPerfil, plainCidadePerfil,
             plainEstadoPerfil, plainBairroPerfil, plainNumPerfil, plainLogradouroPerfil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,14 +49,21 @@ public class Tela_Perfil extends AppCompatActivity {
         BtnVoltaPerfil();
         ImgBtnEdit(solicitacao);
 
-
         carregarDadosPerfil();
         carregarDadosResidencia(solicitacao, getIdClienteSharedPreferences());
     }
+
     private int getIdClienteSharedPreferences() {
-        SharedPreferences ler = getSharedPreferences("usuario", MODE_PRIVATE);
-        return ler.getInt("codigo", 0); // 0, pois sempre terá apenas um usuário cadastrado
+        try {
+            ler = getSharedPreferences("usuario", MODE_PRIVATE);
+            return getSharedPreferences("usuario", MODE_PRIVATE).getInt("codigo", 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(Tela_Perfil.this, "Erro ao carregar seus dados de perfil. Por favor, tente logar novamente ou entre em contato com o suporte.", Toast.LENGTH_SHORT).show();
+            return 0;
+        }
     }
+
     private void ImgBtnEdit(RequestQueue solicitacao) {
         imgBtnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,20 +71,29 @@ public class Tela_Perfil extends AppCompatActivity {
 
                 String ImgAtual = imgBtnEdit.getTag().toString();
 
-                if (!ImgAtual.equals("ImgEditar")) {
+                if (ImgAtual.equals("ImgEditar")) {
+                    imgBtnEdit.setImageResource(R.drawable.iconeok);
+                    imgBtnEdit.setTag("ImgConfirmar");
+                    emailSemAlteracao = plainPerfilE.getText().toString();
+                    telSemAlteracao = plainPerfilTel.getText().toString();
+                    plainPerfilE.setEnabled(true);
+                    plainPerfilTel.setEnabled(true);
+                } else if (!emailSemAlteracao.equals(plainPerfilE.getText().toString()) || !telSemAlteracao.equals(plainPerfilTel.getText().toString())) {
                     dados = new AlterarCadastro(plainPerfilE.getText().toString(), plainPerfilTel.getText().toString());
-                    alterarCadastro(solicitacao, dados, ler);
+                    editarCadastro(solicitacao, dados, ler);
                     imgBtnEdit.setImageResource(R.drawable.edit_perfil);
                     imgBtnEdit.setTag("ImgEditar");
                 } else {
-                    imgBtnEdit.setImageResource(R.drawable.iconeok);
-                    imgBtnEdit.setTag("ImgConfirmar");
-                    plainPerfilE.setEnabled(true);
-                    plainPerfilTel.setEnabled(true);
+                    Toast.makeText(Tela_Perfil.this, "Você não realizou alterações", Toast.LENGTH_SHORT).show();
+                    imgBtnEdit.setImageResource(R.drawable.edit_perfil);
+                    imgBtnEdit.setTag("ImgEditar");
+                    plainPerfilE.setEnabled(false);
+                    plainPerfilTel.setEnabled(false);
                 }
             }
         });
     }
+
     private void BtnVoltaPerfil() {
         btnVoltaPerfil.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,6 +103,7 @@ public class Tela_Perfil extends AppCompatActivity {
             }
         });
     }
+
     private void inicializarViews() {
         btnVoltaPerfil = findViewById(R.id.btnVoltaPerfil);
         imgBtnEdit = findViewById(R.id.imgBtnEdit);
@@ -97,34 +118,49 @@ public class Tela_Perfil extends AppCompatActivity {
         plainLogradouroPerfil = findViewById(R.id.plainLogradouroPerfil);
         spinnerCasas = findViewById(R.id.spinnerCasas);
     }
+
     private void carregarDadosPerfil() {
+        try {
+            ler = getSharedPreferences("usuario", MODE_PRIVATE);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(Tela_Perfil.this, "Erro ao carregar seus dados de perfil. Por favor, tente logar novamente ou entre em contato com o suporte.", Toast.LENGTH_SHORT).show();
+        }
         String nomeCompleto = ler.getString("nome", "") + " " + ler.getString("sobrenome", "");
         txtNome.setText(nomeCompleto);
         plainPerfilE.setText(ler.getString("email", ""));
         plainPerfilTel.setText(ler.getString("telefone", ""));
     }
-    public void alterarCadastro(RequestQueue solicitacao, AlterarCadastro dados, SharedPreferences ler) {
+
+    private void editarCadastro(RequestQueue solicitacao, AlterarCadastro dados, SharedPreferences ler) {
         AlterarCadastro.Alterar(dados, ler.getInt("codigo", 0), solicitacao, new IAlterarCadastro() {
             @Override
             public void onResultado(Cliente clienteAtualizado) {
-                SharedPreferences salvar =
-                        getSharedPreferences("usuario", Context.MODE_PRIVATE);
-                SharedPreferences.Editor gravar = salvar.edit();
-                gravar.putString("nome", clienteAtualizado.getNome());
-                gravar.putString("cpf", clienteAtualizado.getCpf());
-                gravar.putString("email", clienteAtualizado.getEmail());
-                gravar.putString("telefone", clienteAtualizado.getTelefone());
-                gravar.putString("senha", clienteAtualizado.getSenha());
-                gravar.putInt("codigo", clienteAtualizado.getCodigo());
-                gravar.commit();
-                plainPerfilE.setText(clienteAtualizado.getEmail());
-                plainPerfilTel.setText(clienteAtualizado.getTelefone());
-                plainPerfilE.setEnabled(false);
-                plainPerfilTel.setEnabled(false);
+                try {
+                    SharedPreferences salvar =
+                            getSharedPreferences("usuario", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor gravar = salvar.edit();
+                    gravar.putString("nome", clienteAtualizado.getNome());
+                    gravar.putString("cpf", clienteAtualizado.getCpf());
+                    gravar.putString("email", clienteAtualizado.getEmail());
+                    gravar.putString("telefone", clienteAtualizado.getTelefone());
+                    gravar.putString("senha", clienteAtualizado.getSenha());
+                    gravar.putInt("codigo", clienteAtualizado.getCodigo());
+                    gravar.apply();
+                    plainPerfilE.setText(clienteAtualizado.getEmail());
+                    plainPerfilTel.setText(clienteAtualizado.getTelefone());
+                    plainPerfilE.setEnabled(false);
+                    plainPerfilTel.setEnabled(false);
+                    Toast.makeText(Tela_Perfil.this, "Dados alterados com sucesso", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(Tela_Perfil.this, "Erro ao alterar o cadastro. Por favor, tente novamente mais tarde.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
-    public void setTextResidencia(Residencia residenciaSelecionada) {
+
+    private void setTextResidencia(Residencia residenciaSelecionada) {
         plainLogradouroPerfil.setText(residenciaSelecionada.getLogradouro());
         plainCEPPerfil.setText(residenciaSelecionada.getCep());
         plainCidadePerfil.setText(residenciaSelecionada.getMunicipio());
@@ -132,7 +168,8 @@ public class Tela_Perfil extends AppCompatActivity {
         plainNumPerfil.setText("Nº " + residenciaSelecionada.getNumero());
         plainEstadoPerfil.setText(residenciaSelecionada.getUf());
     }
-    public void setEnabledResidencia() {
+
+    private void setEnabledResidencia() {
         plainLogradouroPerfil.setEnabled(false);
         plainCEPPerfil.setEnabled(false);
         plainCidadePerfil.setEnabled(false);
@@ -140,24 +177,32 @@ public class Tela_Perfil extends AppCompatActivity {
         plainNumPerfil.setEnabled(false);
         plainEstadoPerfil.setEnabled(false);
     }
-    public void carregarDadosResidencia(RequestQueue solicitacao, int idCliente) {
+
+    private void carregarDadosResidencia(RequestQueue solicitacao, int idCliente) {
         Residencia.listarResidencias(idCliente, solicitacao, new IResidencia() {
             @Override
             public void onResultado(List<Residencia> residencias) {
-                ResidenciaAdapter adaptador = new ResidenciaAdapter(getApplicationContext(), residencias);
-                spinnerCasas.setAdapter(adaptador);
-                spinnerCasas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                        Residencia residenciaSelecionada = (Residencia) parent.getSelectedItem();
-                        setTextResidencia(residenciaSelecionada);
-                        setEnabledResidencia();
-                    }
-                    @Override
-                    public void onNothingSelected(AdapterView<?> parent) {
-                        setEnabledResidencia();
-                    }
-                });
+                try {
+                    ResidenciaAdapter adaptador = new ResidenciaAdapter(getApplicationContext(), residencias);
+                    spinnerCasas.setAdapter(adaptador);
+                    spinnerCasas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            Residencia residenciaSelecionada = (Residencia) parent.getSelectedItem();
+                            setTextResidencia(residenciaSelecionada);
+                            setEnabledResidencia();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+                            setEnabledResidencia();
+                        }
+
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(Tela_Perfil.this, "Erro ao carregar dados da residencia. Por favor, tente novamente mais tarde.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
