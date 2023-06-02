2c8d64e47fddbf43ed25d903351fbc37fbb04569
Igor12x <igorbtenorioidi@gmail.com>
2023-06-02 02:24:51 -0300

    [feat] na Tela_Carregamento criei a funcionalidade de verificar se possui usuario no sharedPreferences, caso sim, já realizar o login

--- app/src/main/java/com/example/projetotcc/Tela_Carregamento.java
+++ app/src/main/java/com/example/projetotcc/Tela_Carregamento.java
@@ -1,12 +1,20 @@
 package com.example.projetotcc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import Interfaces.ILoginCliente;
+import Models.Cliente;
+import Models.ClienteLogin;
+import Models.Residencia;
 
 public class Tela_Carregamento extends AppCompatActivity {
 
@@ -15,11 +23,63 @@ public class Tela_Carregamento extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_carregamento);
 
-        Handler handler = new Handler();
-        handler.postDelayed(() -> {
-        Intent intent = new Intent(getApplicationContext(), Tela_Inicial.class);
-        startActivity(intent);
-        }, 4000);
+        RequestQueue solicitacao = Volley.newRequestQueue(this);
+        logar(solicitacao);
+    }
 
+    private void logar(RequestQueue solicitacao){
+        if(!possuiUsuarioGravado()) {
+            Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                Intent intent = new Intent(getApplicationContext(), Tela_Inicial.class);
+                startActivity(intent);
+            }, 4000);
+        } else {
+            SharedPreferences ler = getSharedPreferences("usuario", MODE_PRIVATE);
+            ValidarLogin(solicitacao, ler.getString("cpf", ""), ler.getString("senha", ""));
+        }
+    }
+    private boolean possuiUsuarioGravado() {
+        try {
+            SharedPreferences sharedPreferences = getSharedPreferences("usuario", MODE_PRIVATE);
+            return sharedPreferences.contains("nome");
+        } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+    private void abrirTelaPrincipal() {
+        Intent intent = new Intent(getApplicationContext(), Tela_Principal.class);
+        startActivity(intent);
+    }
+    private void abrirTelaLogin() {
+        Intent intent = new Intent(getApplicationContext(), Tela_Login.class);
+        startActivity(intent);
+    }
+    public void ValidarLogin(RequestQueue solicitacao, String cpf, String senha) {
+        ClienteLogin login = new ClienteLogin(cpf, senha);
+        ClienteLogin.ValidarLoginCliente(login, solicitacao, new ILoginCliente() {
+            @Override
+            public void onResultado(Cliente clienteLogado) {
+                verificarResidenciaCadastrada(solicitacao, clienteLogado.getCpf());
+            }
+
+            @Override
+            public void onErro(String mensagemErro) {
+                Toast.makeText(Tela_Carregamento.this, mensagemErro, Toast.LENGTH_SHORT).show();
+                abrirTelaLogin();
+            }
+        });
+    }
+
+    public void verificarResidenciaCadastrada(RequestQueue solicitacao, String cpf) {
+        Residencia.verificarResidenciaCadastrada(cpf, solicitacao, cadastrado -> {
+            if (cadastrado) {
+                abrirTelaPrincipal();
+            } else {
+                Toast.makeText(Tela_Carregamento.this, "Você ainda não possui nosso medidor inteligente, aguarde o contato do nosso consultor", Toast.LENGTH_SHORT).show();
+                abrirTelaLogin();
+            }
+        });
     }
 }
